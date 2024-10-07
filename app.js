4e9ffc7957e2fd1e6d7609c05aa6aeda825ed2da
marcola47 <mekaminski47@gmail.com>
2024-10-07 14:19:20 -0300

    .

---
+++ app.js
@@ -0,0 +1,43 @@
+const prompt = require('prompt-sync')();
+
+let xp, rank;
+const name = prompt("Qual seu nome? ")
+
+do {
+  xp = prompt("Qual seu nivel de XP? (Somente números) ");
+} while ((/[a-zA-Z]/).test(xp))
+
+switch (true) {
+  case xp < 1000: 
+    rank = "Ferro"; 
+    break;
+
+  case xp < 2000: 
+    rank = "Bronze"; 
+    break;
+
+  case xp < 5000: 
+    rank = "Prata"; 
+    break;
+
+  case xp < 7000: 
+    rank = "Ouro"; 
+    break;
+
+  case xp < 8000: 
+    rank = "Platina"; 
+    break;
+
+  case xp < 9000: 
+    rank = "Ascendente"; 
+    break;
+
+  case xp < 10000: 
+    rank = "Imortal"; 
+    break;
+
+  default: 
+    rank = "Radiante";
+}
+
+console.log(`O Herói de nome ${name} está no nível de ${rank}`);
